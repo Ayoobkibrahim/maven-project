f8802b25c24b3e1a3e6cf209477978a051787aef
Ayoobkibrahim <ayoobkibrahim01.com>
2025-10-11 15:44:57 +0530

    Create a unit test code java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,38 +1,21 @@
 package com.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+        App app = new App();
+        int result = app.add(5, 3);
+        assertEquals(8, result, "5 + 3 should equal 8");
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testAddNegative() {
+        App app = new App();
+        int result = app.add(-2, -3);
+        assertEquals(-5, result, "-2 + -3 should equal -5");
     }
 }
