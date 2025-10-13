13d317c1458a03f8d33f57503c62ce8d092315b5
Ayoobkibrahim <ayoobkibrahim01.com>
2025-10-13 13:13:38 +0530

    app.java change printing hello word to Hello Maven Project!

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Maven Project!" );
     }
 
     public int add(int a,int b){
