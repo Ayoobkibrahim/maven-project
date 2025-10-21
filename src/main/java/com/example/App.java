2847396abab92eeeb300288fac3058fb9507b265
Ayoobkibrahim <ayoobkibrahim01.com>
2025-10-21 18:11:36 +0530

    remove testing display message in app.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,7 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Maven Project!" );
-        System.out.println( "Hi Welcome My Maven Project" );
     }
 
     public int add(int a,int b){
