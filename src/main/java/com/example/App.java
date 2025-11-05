a7aa7241f58927ae1e412ab92d370fd1216a31fc
Ayoobkibrahim <ayoobkibrahim01.com>
2025-11-05 23:54:38 +0530

    redo the project in to previous using

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,27 +1,17 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@SpringBootApplication
-@RestController
-public class App {
-
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World Maven Project!" );
     }
 
-    @GetMapping("/")
-    public String home() {
-        return "Hello World from Spring Boot Web App!";
-    }
-
-    @GetMapping("/add")
-    public String addNumbers(@RequestParam int a, @RequestParam int b) {
-        int result = a + b;
-        return "Sum of " + a + " and " + b + " is: " + result;
+    public int add(int a,int b){
+        return a+b;
     }
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,23 +1,21 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AppTest {
 
-    @Autowired
-    private TestRestTemplate restTemplate;
+    @Test
+    public void testAdd() {
+        App app = new App();
+        int result = app.add(5, 3);
+        assertEquals(8, result, "5 + 3 should equal 8");
+    }
 
     @Test
-    public void testAddEndpoint() {
-        ResponseEntity<String> response =
-                restTemplate.getForEntity("/add?a=5&b=3", String.class);
-        assertTrue(response.getBody().contains("8"), "Sum should be 8");
+    public void testAddNegative() {
+        App app = new App();
+        int result = app.add(-2, -3);
+        assertEquals(-5, result, "-2 + -3 should equal -5");
     }
 }
