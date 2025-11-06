449aa0a619a8580adea74eb1b3fd218edbcd3bd5
Ayoobkibrahim <ayoobkibrahim01.com>
2025-11-06 12:44:16 +0530

    Java service, create a small web server instead of a one-shot message

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,17 +1,23 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World Maven Project!" );
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        System.out.println("✅ Java server is running on port 8080...");
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", exchange -> {
+            String response = "Hello World Maven Project!";
+            exchange.sendResponseHeaders(200, response.length());
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.close();
+        });
+        server.start();
     }
 
-    public int add(int a,int b){
-        return a+b;
+    public int add(int a, int b) {
+        return a + b;
     }
 }
